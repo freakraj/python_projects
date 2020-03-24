05f3efbcb89675f322d03f65e87de34f20aaf6e8
freakraj <raj0042gautam@gmail.com>
2020-03-24 18:59:13 +0530

    If else Istatement in Python

---
+++ if_statement.py
@@ -0,0 +1,13 @@
+# syntex 
+# if condition :
+ # if codition is true then this code will be execute 
+ # code 
+
+age =input("Please Enter Your age : ")
+
+age=int(age)
+
+if age>=14:
+    print("You are eligible play in game")
+else:
+    print("your are not eligible to game")
