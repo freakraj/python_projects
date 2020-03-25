bd3fa29d878e70dbe984f0f77fc11b3afd4bd4f8
freakraj <raj0042gautam@gmail.com>
2020-03-25 13:00:21 +0530

    If else Statement in python

--- if_statement.py
+++ if_statement.py
@@ -3,11 +3,20 @@
  # if codition is true then this code will be execute 
  # code 
 
-age =input("Please Enter Your age : ")
+# age =input("Please Enter Your age : ")
 
-age=int(age)
+# age=int(age)
 
-if age>=14:
-    print("You are eligible play in game")
+# if age>=14:
+#     print("You are eligible play in game")
+# else:
+#     print("your are not eligible to game")
+
+age=input("Please enter your are : ")
+
+age =int(age)
+
+if age>=15 :
+    print("You are elgible pay the gaame")
 else:
-    print("your are not eligible to game")
+     print("You are not eligble pay the game")
